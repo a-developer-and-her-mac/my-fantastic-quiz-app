1db0f2f9fbc3d6d24a7ebf15945d24069b77de99
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2020-10-07 10:00:00 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { QuestionsService } from './questions.service';
+import { Quiz, Answers, Choice } from './quiz.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-fantastic-quiz-app';
+  answers: Answers;
+  quiz: Quiz;
+  currentQuestionIndex: number;
+  private showResults = false;
+
+  constructor(private questionsService: QuestionsService) {
+    this.questionsService.getJSON('norwegian').subscribe((data) => {
+      this.quiz = new Quiz('norwegian', data);
+      this.answers = new Answers();
+      this.currentQuestionIndex = 0;
+    });
+  }
+
+  updateChoice(choice: Choice) {
+    this.answers.values[this.currentQuestionIndex] = choice;
+  }
+
+  nextOrViewResults() {
+    if (this.currentQuestionIndex === this.quiz.questions.length - 1) {
+      this.showResults = true;
+      return;
+    }
+    this.currentQuestionIndex++;
+  }
 }
